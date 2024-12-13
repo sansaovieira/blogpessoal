61d97b379e3d5bd01031f6054fa2166e6a74cf8a
sansaovieira <vieirasansao42@gmail.com>
2024-12-13 05:40:40 -0300

    fear(PersonalPessoal): Creates the project with all classes and settings

---
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -0,0 +1,68 @@
+/*
+ * 
+ * package com.generation.blogpessoal.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+    OpenAPI springBlogPessoalOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Personal Blog - Posts API")
+                .description("This project is part of **Generation Brasil's** **Bootcamp**, which was a challenge presented after the backend block, and is focused on developing a fully functional personal blog application. The backend provides a robust API for managing blog posts and serving as a learning platform to implement best practices in software development, architecture, and teamwork.\r\n"
+                		+ "")
+                .version("v0.0.1")
+                .license(new License()
+                    .name("Generation Brasil")
+                    .url("https://brazil.generation.org/"))
+                .contact(new Contact()
+                    .name("Sansão Dembué Vieira")
+                    .url("https://github.com/sansaovieira")
+                    .email("vieirasansao42@gmail.com")))
+            .externalDocs(new ExternalDocumentation()
+                .description("Github")
+                .url("https://github.com/sansaovieira/blogpessoal/tree/main"));
+    }
+
+
+	@Bean
+	OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+				ApiResponses apiResponses = operation.getResponses();
+
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+			}));
+		};
+	}
+
+	private ApiResponse createApiResponse(String message) {
+
+		return new ApiResponse().description(message);
+
+	}
+}*/
+ 
